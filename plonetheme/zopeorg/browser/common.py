4a89a102130e0e69a1974e28c8ae8dc78d19adca
Johannes Raggam <raggam-nl@adm.at>
2010-10-11 18:30:15 +0200

    deleted unused

--- plonetheme/zopeorg/browser/common.py
+++ plonetheme/zopeorg/browser/common.py
@@ -2,9 +2,13 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
-class FooterViewlet(ViewletBase):
-    index = ViewPageTemplateFile('templates/footer.pt')
+from zope.interface import Interface
 
+class IZopeorgTheme(Interface):
+    """Zope.org theme layer
+    """
+
+class FooterViewlet(ViewletBase):
     def update(self):
         super(FooterViewlet, self).update()
         self.navigation_root_url = self.portal_state.navigation_root_url()
--- plonetheme/zopeorg/browser/interfaces.py
+++ plonetheme/zopeorg/browser/interfaces.py
@@ -6,10 +6,6 @@ class IZopeorgTheme(Interface):
     """Marker interface that defines a Zope 3 browser layer.
     """
 
-class IPagePortlets(IPortletManager):
-    """we need our own portlet manager above the content area.
-    """
-
 class IFrontPage(Interface):
     """ This is a marker interface for my frontpage
     """
