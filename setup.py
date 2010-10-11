439a6bf91467925490173fe7d734a32fee5c8b9c
Johannes Raggam <raggam-nl@adm.at>
2010-10-11 18:42:21 +0200

    cleanup, bugfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.0-dev'
 
 setup(name='plonetheme.zopeorg',
       version=version,
-      description="Plone theme for www.zope.org",
+      description="zope.org Plone theme",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?:action=list_classifiers
