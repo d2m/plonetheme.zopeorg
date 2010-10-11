bb42b7d839a947e6b5b243c72cecd8e6f51f5b12
Johannes Raggam <raggam-nl@adm.at>
2010-10-11 17:24:31 +0200

    cleanup

--- plonetheme/zopeorg/browser/interfaces.py
+++ plonetheme/zopeorg/browser/interfaces.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
-from plone.theme.interfaces import IDefaultPloneLayer
 from zope.interface import Interface
+from plone.portlets.interfaces import IPortletManager
 
-class IThemeSpecific(IDefaultPloneLayer):
+class IZopeorgTheme(Interface):
     """Marker interface that defines a Zope 3 browser layer.
     """
 
-from plone.portlets.interfaces import IPortletManager
-
-
-
 class IPagePortlets(IPortletManager):
-
     """we need our own portlet manager above the content area.
-
     """
 
 class IFrontPage(Interface):
