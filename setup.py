08662a39c0870861aff79714f5783287ccf705b7
Johannes Raggam <raggam-nl@adm.at>
2010-10-13 18:24:40 +0200

    don't include dependency canditates likely to be removed

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ setup(name='plonetheme.zopeorg',
           # -*- Extra requirements: -*-
           'Products.Collage',
           'collective.collage.portlets',
-          'collective.easyslider',
-          'collective.collage.easyslider',
-          'Products.PloneSoftwareCenter',
+          #'collective.easyslider',
+          #'collective.collage.easyslider',
+          #'Products.PloneSoftwareCenter',
       ],
       )
