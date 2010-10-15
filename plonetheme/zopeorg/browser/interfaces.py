5c61e33ea8840fe7165d23dfb6baf9e7769080e4
Johannes Raggam <raggam-nl@adm.at>
2010-10-15 18:39:09 +0200

    remove unused. remove interfaces file if the interface in there will be the only one. use def from common.py

--- plonetheme/zopeorg/browser/interfaces.py
+++ plonetheme/zopeorg/browser/interfaces.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 from zope.interface import Interface
-from plone.portlets.interfaces import IPortletManager
 
 class IZopeorgTheme(Interface):
     """Marker interface that defines a Zope 3 browser layer.
     """
-
-class IFrontPage(Interface):
-    """ This is a marker interface for my frontpage
-    """
-
-
