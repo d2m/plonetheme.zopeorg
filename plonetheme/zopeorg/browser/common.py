2eace78b3f1e1c554091c3cfdbec4853aae0b830
Johannes Raggam <raggam-nl@adm.at>
2010-10-15 18:37:59 +0200

    add teaserview stub

--- plonetheme/zopeorg/browser/common.py
+++ plonetheme/zopeorg/browser/common.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
+from Products.Five.browser import BrowserView
 
 from zope.interface import Interface
 
@@ -12,3 +13,10 @@ class FooterViewlet(ViewletBase):
     def update(self):
         super(FooterViewlet, self).update()
         self.navigation_root_url = self.portal_state.navigation_root_url()
+
+class CollageTeaserView(BrowserView):
+    title = u"Teaser View"
+
+    @property
+    def get_teasers(self):
+        return []
