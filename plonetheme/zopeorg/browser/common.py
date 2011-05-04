c25d0543a4f01784ced8d58a44c9528c6b000ee6
Johannes Raggam <raggam-nl@adm.at>
2011-05-05 01:53:36 +0200

    remove teaser view, since teaser is used as portlet yet

--- plonetheme/zopeorg/browser/common.py
+++ plonetheme/zopeorg/browser/common.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
-from Products.Five.browser import BrowserView
-from Products.CMFCore.utils import getToolByName
-from collective.teaser.browser.common import get_teasers
-
 from zope.interface import Interface
+from plone.app.layout.viewlets.common import ViewletBase
 
 class IZopeorgTheme(Interface):
     """Zope.org theme layer
@@ -15,10 +9,3 @@ class FooterViewlet(ViewletBase):
     def update(self):
         super(FooterViewlet, self).update()
         self.navigation_root_url = self.portal_state.navigation_root_url()
-
-class CollageTeaserView(BrowserView):
-    title = u"Teaser View"
-
-    @property
-    def teasers(self):
-        return get_teasers(self.context, self.context.data, self.request)
