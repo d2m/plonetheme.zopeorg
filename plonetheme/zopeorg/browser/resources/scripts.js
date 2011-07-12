0be18f3e38fefa4b655fe7fde6dccf99306015c8
Johannes Raggam <raggam-nl@adm.at>
2011-07-12 13:42:35 +0200

    automatically set accordion for h2.accordion headers

--- plonetheme/zopeorg/browser/resources/scripts.js
+++ plonetheme/zopeorg/browser/resources/scripts.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-    $("#accordion").accordion({ autoHeight: false });
-});
+(function($) {
+    $(document).ready(function() {
+        var tagName = 'h2.accordion';
+        if ($("#parent-fieldname-text " + tagName).length>0 && !$("#document-base-edit").length>0) {
+            $(tagName).each(function () {
+                var $this = $(this).next();
+                $this.add($this.nextUntil(tagName)).wrapAll('<div></div>');
+            });
+            $("#parent-fieldname-text").children().wrapAll('<div id="accordion"></div>');
+            $("#accordion").accordion({ header: tagName, autoHeight: false });
+        }
+    });
+})(jQuery);
